b87ca5a99ba3c859275f8a18da7b8d9cc6ff9070
Andrey Khomyakov <yourwebwizard@gmail.com>
2020-12-10 20:25:34 +0300

    Test task files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+
+	public function run()
+	{
+		Model::unguard();
+
+		$this->call(IngredientTableSeeder::class);
+		$this->command->info('Ingredient table seeded!');
+
+		$this->call(RecipeTableSeeder::class);
+		$this->command->info('Recipe table seeded!');
+
+		$this->call(RecipeIngredientTableSeeder::class);
+		$this->command->info('RecipeIngredient table seeded!');
+	}
 }
